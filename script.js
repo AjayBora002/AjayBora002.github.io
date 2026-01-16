7e92c66d3b9e310a42d57a1a194f4d1b6b91f5c3
Ajay Bora <boraajay26@gmail.com>
2026-01-16 21:26:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Optional: Mobile menu toggle logic can be added here
